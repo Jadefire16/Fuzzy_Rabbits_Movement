f32ca1fb8635df0c3e6a2b8d52d73841ee83f49a
Jadefire16 <53870574+Jadefire16@users.noreply.github.com>
2020-01-16 01:34:20 -0500

    Refined Sliding
    Refined basic sliding mechanic Known issues
    ~ Jumping and hitting slide will not initiate slide even if target will land on a sloped surface
    ~Bouncing when sliding is enabled

--- CharacterMovement/Assets/SlideScript.cs
+++ CharacterMovement/Assets/SlideScript.cs
@@ -9,7 +9,13 @@ public class SlideScript : MonoBehaviour
     CrouchScript crouch;
     MovementController controller;
     CharacterController charController;
+    Transform castPoint;
     [SerializeField] List<PhysicMaterial> physMat = new List<PhysicMaterial>();
+    [SerializeField] private float slideForce = 4.5f; // used to continue momentum when entering slide mode 4.5f seeems good
+
+    public LayerMask antiSlideMask;
+
+    bool isSliding = false;
 
     void Start()
     {
@@ -21,6 +27,8 @@ public class SlideScript : MonoBehaviour
             controller = GetComponent<MovementController>();
         if (!charController)
             charController = GetComponent<CharacterController>();
+        if (!castPoint)
+            castPoint = controller.groundCheck;
         rb.useGravity = false;
         rb.detectCollisions = false;
         rb.isKinematic = true;
@@ -29,46 +37,55 @@ public class SlideScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.LeftShift) && controller.IsGrounded)
         {
-            crouch._collider.material = physMat[1];
-
-            controller.enabled = false;
-            crouch.enabled = false;
-            charController.enabled = false;
-            EnableRB();
+            EnableSlide();
         }
         else if (Input.GetKeyUp(KeyCode.LeftShift))
         {
-            crouch._collider.material = physMat[0];
-
-            DisableRB();
-            controller.enabled = true;
-            crouch.enabled = true;
-            charController.enabled = true;
+            DisableSlide();
         }
+
+        Ray ray = new Ray(castPoint.position, -castPoint.up);
+        if (Physics.Raycast(ray, out RaycastHit info, 0.25f, antiSlideMask, QueryTriggerInteraction.Ignore))
+        {
+            DisableSlide();
+        }
+
 
     }
 
-    void DisableRB()
+    void DisableSlide()
     {
         if (!rb)
             return;
+        controller.enabled = true;
+        crouch.enabled = true;
+        charController.enabled = true;
+        crouch._collider.material = physMat[0];
+        isSliding = false;
         rb.isKinematic = true;
         rb.detectCollisions = false;
         rb.useGravity = false;
         rb.Sleep();
     }
 
-    void EnableRB()
+    void EnableSlide()
     {
         if (!rb)
             return;
+        crouch._collider.material = physMat[1];
+        controller.enabled = false;
+        crouch.enabled = false;
+        charController.enabled = false;
+        isSliding = true;
         rb.isKinematic = false;
         rb.detectCollisions = true;
         rb.useGravity = true;
         rb.constraints = RigidbodyConstraints.FreezeRotation;
         rb.WakeUp();
+
+        rb.AddForce(transform.forward * slideForce, ForceMode.Impulse);
     }
 
 }
